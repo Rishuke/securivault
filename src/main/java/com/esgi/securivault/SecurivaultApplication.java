79e31a2c62eb32796c6d4ec6792d8ba3862fa54f
rishi <balasubramaniam.rishi@gmail.com>
2025-05-19 16:13:30 +0200

    Add initialize of the Springboot project API.

---
+++ src/main/java/com/esgi/securivault/SecurivaultApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.securivault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurivaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurivaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgi/securivault/SecurivaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.securivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurivaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
